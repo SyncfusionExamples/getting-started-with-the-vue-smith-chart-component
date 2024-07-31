f6c5b6e83c683bcdaf5b8e8de8040ea244c2f104
Niluba <139118395+Niluba@users.noreply.github.com>
2024-07-31 14:17:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# getting-started-with-the-vue-smith-chart-component
-A quick start project that shows how to create and configure the Syncfusion Vue Smith Chart component in a Vue application. This sample explains how to populate list data, add legends, and enable data labels and tooltips.
+# Getting Started with the Vue Smith Chart Component
+
+A quick start project that shows how to create and configure the Syncfusion Vue Smith Chart component in a Vue application. This sample explains how to populate list data, add legends, and enable data labels and tooltips.   
+ 
+Refer to the following documentation to learn about the Vue Smith Chart component: 
+https://ej2.syncfusion.com/vue/documentation/smithchart/vue-3-getting-started  
+
+Check out this online example of the Vue Smith Chart component:
+https://ej2.syncfusion.com/vue/demos/#/bootstrap5/smith-chart/default.html
+
+## Project prerequisites
+Make sure that you have the compatible versions of [Visual Studio Code](https://code.visualstudio.com/download ) and [NodeJS](https://nodejs.org/en/download) or later version in your machine before starting to work on this project.
+
+## How to run this application
+To run this application, you need to first clone the `getting-started-with-the-vue-smith-chart-component` repository and then open it in Visual Studio Code. Now, simply build and run your project to view the output.
---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {SmithchartComponent as EjsSmithchart, SeriesCollectionDirective as ESeriesCollection,
+        SeriesDirective as ESeries, SmithchartLegend, TooltipRender
+} from '@syncfusion/ej2-vue-charts';
+import {provide} from 'vue';
+
+provide('smithchart',[SmithchartLegend, TooltipRender]);
+
+const points = [
+{ resistance: 20, reactance: -50 }, { resistance: 10, reactance: -10 },
+{ resistance: 9, reactance: -4.5 }, { resistance: 8, reactance: -3.5 },
+{ resistance: 7, reactance: -2.5 }, { resistance: 6, reactance: -1.5 },
+{ resistance: 5, reactance: -1 }, { resistance: 4.5, reactance: -0.5 },
+{ resistance: 3.5, reactance: 0 }, { resistance: 2.5, reactance: 0.4 },
+{ resistance: 2, reactance: 0.5 }, { resistance: 1.5, reactance: 0.5 },
+{ resistance: 1, reactance: 0.4 }, { resistance: 0.5, reactance: 0.2 },
+{ resistance: 0.3, reactance: 0.1 }, { resistance: 0, reactance: 0.05 }
+];
+const dataSource = [
+{ resistance: 10, reactance: 25 }, { resistance: 8, reactance: 6 },
+{ resistance: 6, reactance: 4.5 }, { resistance: 4.5, reactance: 2 },
+{ resistance: 3.5, reactance: 1.6 }, { resistance: 2.5, reactance: 1.3 },
+{ resistance: 2, reactance: 1.2 }, { resistance: 1.5, reactance: 1 },
+{ resistance: 1, reactance: 0.8 }, { resistance: 0.5, reactance: 0.4 },
+{ resistance: 0.3, reactance: 0.2 }, { resistance: 0, reactance: 0.15 }
+];
+const title = {text:'Impedance Transmission'};
+const marker = {visible: true, dataLabel:{visible: true}};
+const marker2={visible: true};
+const legendSettings = {visible: true};
+const tooltip = {visible: true};
+
+</script>
+
+<template>
+<ejs-smithchart :title="title" :legendSettings="legendSettings">
+  <e-seriesCollection>
+    <e-series :points="points" :marker="marker"
+    name="Transmission1" :tooltip="tooltip"></e-series>
+    <e-series :dataSource="dataSource" resistance="resistance"
+    reactance="reactance" :marker="marker2" name="Transmission2"
+    :tooltip="tooltip"></e-series>
+  </e-seriesCollection>
+</ejs-smithchart>
+</template>
+
+<style scoped>
+
+</style>
